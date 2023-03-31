50dfdfa3bd4a8e5eaf9ce759cf64a0c919a8fb4e
GustavoDalpiaz <gustavo_dalpiaz@unoesc.edu.br>
2023-03-30 21:10:28 -0300

    Feito o codigo para informar a data e hora.

--- src/projetosGit/TesteGit.java
+++ src/projetosGit/TesteGit.java
@@ -1,9 +1,21 @@
 package projetosGit;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
 public class TesteGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	
+		DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL);
+		
+		Date data1= new Date();
+		int horas = data1.getHours();
+		int minutos = data1.getMinutes();
+		System.out.println("Hoje é "+df4.format(data1)+", e agora são "+ horas +" horas e "+minutos+" minutos" );
 
 	}
 
